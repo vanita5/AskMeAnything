c1a4596c4f69e86649fd622051350fd5bba71e46
vanita5 <mail@vanita5.de>
2014-11-16 06:35:05 +0100

    add basic functionality and tweeting

--- webForm/ask.py
+++ webForm/ask.py
@@ -1,12 +1,33 @@
-from flask import Flask, render_template, request
+import sys
+import config
+import tweepy
+from flask import Flask, render_template, request, redirect, url_for
 
-# init
+#init tweepy
+try:
+    auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
+    auth.set_access_token(config.access_token, config.access_token_secret)
+    twitter = tweepy.API(auth)
+    print "Skipped Twitter"
+except:
+    print "Twitter authentication failed!"
+    sys.exit(1)
+
+# init Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def ask():
-    return render_template('ask.html')
+    asked = False
+    error = False
+    if request.method == 'GET':
+        if request.values.get('asked') == '1':
+            asked = True
+        elif request.values.get('error') == '1':
+            error = True
+
+    return render_template('ask.html', name=config.USERNAME, asked=asked, error=error)
 
 
 @app.route('/answers')
@@ -21,9 +42,25 @@ def faq():
 
 @app.route('/ask', methods=['POST', 'GET'])
 def doAsk():
-    if request.method == 'POST':
-        #TODO add question
-        return None
+    try:
+        if request.method == 'POST':
+            question = request.form.get('q')
+
+            # Validate length
+            if len(question) > 130:
+                raise
+
+            # Send to Twitter
+            result = twitter.update_status('@' + config.SCREENNAME + ' ' + question)
+
+            tweet_id = result.id_str
+
+
+            # TODO persist in database
+
+        return redirect('/?asked=1')
+    except Exception as e:
+        return redirect('/?error=1')
 
 
 if __name__ == '__main__':
